29d2ed29515b80821d9a364c54824a4f3dc7374e
lvjianxiong <lyujianxiong@totok.ai>
2020-08-04 21:06:21 +0800

    纹理倒置解决方案1

---
+++ TextureTurn1/TargetView.h
@@ -0,0 +1,21 @@
+//
+//  TargetView.h
+//  TextureTurn1
+//
+//  Created by lvjianxiong on 2020/8/4.
+//  Copyright © 2020 lvjianxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TargetView : UIView
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
